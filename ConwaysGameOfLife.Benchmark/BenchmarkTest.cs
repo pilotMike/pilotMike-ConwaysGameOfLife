fe74342fb288d9d9e5184723faf038704b7d3e86
Michael DiLeo <m_dileo@live.com>
2020-07-11 02:00:23 -0500

    merge and uncomment benchmarks

--- ConwaysGameOfLife.Benchmark/BenchmarkTest.cs
+++ ConwaysGameOfLife.Benchmark/BenchmarkTest.cs
@@ -2,18 +2,15 @@
 using ConwaysGameOfLife.GoL;
 using ConwaysGameOfLife.Grids;
 using ConwaysGameOfLife.Views;
-using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftWindowsTCPIP;
 using System;
 using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConwaysGameOfLife.Benchmark
 {
     [MemoryDiagnoser]
     public class BenchmarkTest
     {
-        [Params(/*50,*/ /*100,*/ 300)]
+        [Params(50, 100, 300)]
         public int Dimensions { get; set; }
         public int Iterations { get; set; } = 1000;
 
@@ -41,50 +38,50 @@ namespace ConwaysGameOfLife.Benchmark
             gol.Run(hs, buffer, Iterations);
         }
 
-        //[Benchmark]
-        //public void ParallelHashSet()
-        //{
-        //    var grid = new ParallelHashGrid(_state);
-        //    var gol = new ParallelGameOfLife();
-        //    gol.Run<ParallelHashGrid, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
-        //}
+        [Benchmark]
+        public void ParallelHashSet()
+        {
+            var grid = new ParallelHashGrid(_state);
+            var gol = new ParallelGameOfLife();
+            gol.Run<ParallelHashGrid, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
+        }
 
-        //[Benchmark]
-        //public void ParallelAggressiveOptimization()
-        //{
-        //    var grid = new ParallelHashGrid(_state);
-        //    var gol = new AggressiveParallelGameOfLife();
-        //    gol.Run<ParallelHashGrid, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
-        //}
+        [Benchmark]
+        public void ParallelAggressiveOptimization()
+        {
+            var grid = new ParallelHashGrid(_state);
+            var gol = new AggressiveParallelGameOfLife();
+            gol.Run<ParallelHashGrid, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
+        }
 
-        //[Benchmark]
-        //public void ParallelBuffered()
-        //{
-        //    var grid = new BufferedParallelHashGrid(_state);
-        //    var gol = new ParallelGameOfLife_Buffered();
-        //    gol.Run<BufferedParallelHashGrid, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
-        //}
+        [Benchmark]
+        public void ParallelBuffered()
+        {
+            var grid = new BufferedParallelHashGrid(_state);
+            var gol = new ParallelGameOfLife_Buffered();
+            gol.Run<BufferedParallelHashGrid, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
+        }
 
-        //[Benchmark]
-        //public void AggressiveParallelRemoveStackAllocForeach()
-        //{
-        //    var grid = new ParallelHashGrid_NoStackAlloc(_state);
-        //    var gol = new ParallelGameOfLife();
-        //    gol.Run<ParallelHashGrid_NoStackAlloc, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
-        //}
+        [Benchmark]
+        public void AggressiveParallelRemoveStackAllocForeach()
+        {
+            var grid = new ParallelHashGrid_NoStackAlloc(_state);
+            var gol = new ParallelGameOfLife();
+            gol.Run<ParallelHashGrid_NoStackAlloc, NullView>(grid, new GameOfLifeOptions { MaxIterations = Iterations });
+        }
 
-        //[Benchmark]
-        //public void ForLoop()
-        //{
-        //    var gol = new ForLoopGoL();
-        //    gol.Run<NullView, List<Coordinate>>(_state, Dimensions, new GameOfLifeOptions { MaxIterations = Iterations });
-        //}
+        [Benchmark]
+        public void ForLoop()
+        {
+            var gol = new ForLoopGoL();
+            gol.Run<NullView, List<Coordinate>>(_state, Dimensions, new GameOfLifeOptions { MaxIterations = Iterations });
+        }
 
-        //[Benchmark]
-        //public void ParallelForLoop()
-        //{
-        //    var gol = new ParallelForLoopGoL();
-        //    gol.Run<NullView, List<Coordinate>>(_state, Dimensions, new GameOfLifeOptions { MaxIterations = Iterations });
-        //}
+        [Benchmark]
+        public void ParallelForLoop()
+        {
+            var gol = new ParallelForLoopGoL();
+            gol.Run<NullView, List<Coordinate>>(_state, Dimensions, new GameOfLifeOptions { MaxIterations = Iterations });
+        }
     }
 }
